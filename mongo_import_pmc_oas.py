f0f0103d636fddeff16a95782c9d44dd39c90603
Toph Allen <toph.allen@gmail.com>
2016-02-03 13:53:16 -0500

    There were some changes to the PMC OAS importer. That is all.

--- mongo_import_pmc_oas.py
+++ mongo_import_pmc_oas.py
@@ -2,14 +2,20 @@
 
 from ftplib import FTP
 
-# Log into FTP server; login; change directory.
-ftp = FTP('ftp.ncbi.nlm.nih.gov')
-ftp.login()
-ftp.cwd('pub/pmc')
 
-for x in ["A-B", "C-H", "I-N", "O-Z"]:
-    filename = "articles.{}.tar.gz".format(x)
-    localfile = open(filename, 'wb')
-    ftp.retrbinary(cmd='RETR {}'.format(filename), callback=localfile.write)
 
-ftp.close()
+
+
+
+if __name__ == '__main__':
+    # Log into FTP server; login; change directory.
+    ftp = FTP('ftp.ncbi.nlm.nih.gov')
+    ftp.login()
+    ftp.cwd('pub/pmc')
+
+    for x in ["A-B", "C-H", "I-N", "O-Z"]:
+        filename = "articles.{}.tar.gz".format(x)
+        localfile = open(filename, 'wb')
+        ftp.retrbinary(cmd='RETR {}'.format(filename), callback=localfile.write)
+
+    ftp.close()
